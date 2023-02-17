6da2fd803ea3007091ff34265ebb5af823ec6f30
William Shepard <60353404+NHGrizzly@users.noreply.github.com>
2023-02-17 12:29:30 -0500

    created sna-sim.py

---
+++ sna-sim.py
@@ -0,0 +1,17 @@
+import pyvisa
+
+rm = pyvisa.ResourceManager(r'C:\Users\wills\OneDrive\Documents\GitHub\rf-testset\hp5787d.yaml@sim')
+# tells pyvisa to use a simulated backend based on the local file rf-inst.yaml using a raw string
+
+# print(rm.list_resources()) # prints all of the visa devices visible to the resource manager
+
+inst = rm.open_resource('GPIB0::8::INSTR', read_termination = '\n', write_termination = '\n', query_delay = 0.1)
+# opens the gpib instrument on board 0 numbered 4 using line separators and a delay for queries
+
+print(inst) # identifies inst from pyvisa
+
+print(inst.query('?IDN')) # self identify inst
+
+print(inst.query('!CAL')) # calibrate the device
+
+print(inst.query('?PENIS')) # harass the instrument
