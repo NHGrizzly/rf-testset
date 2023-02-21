d1c82bb15b56471d69c15edde9cb738700e229ad
William Shepard <60353404+NHGrizzly@users.noreply.github.com>
2023-02-21 12:16:48 -0500

    changed device name to HP5787D
    name needed to be capitalized to match device assignments below

--- sna-sim.py
+++ sna-sim.py
@@ -5,13 +5,15 @@ rm = pyvisa.ResourceManager(r'C:\Users\wills\OneDrive\Documents\GitHub\rf-testse
 
 # print(rm.list_resources()) # prints all of the visa devices visible to the resource manager
 
-inst = rm.open_resource('GPIB0::8::INSTR', read_termination = '\n', write_termination = '\n', query_delay = 0.1)
+inst = rm.open_resource('GPIB0::12::INSTR', read_termination = '\n', write_termination = '\n', query_delay = 0.5)
 # opens the gpib instrument on board 0 numbered 4 using line separators and a delay for queries
 
 print(inst) # identifies inst from pyvisa
 
-print(inst.query('?IDN')) # self identify inst
+print(inst.query('OI')) # self identify inst
 
-print(inst.query('!CAL')) # calibrate the device
+print(inst.query('IP')) # calibrate the device
 
-print(inst.query('?PENIS')) # harass the instrument
+print(inst.query('C1 IA C2 IB'))
+
+# print(inst.query('?PENIS')) # harass the instrument
