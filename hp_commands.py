4c57adaa9be154672b6f8df47a86994d67899dbe
William Shepard <60353404+NHGrizzly@users.noreply.github.com>
2023-02-21 13:49:50 -0500

    add assign() to instrument

--- hp_commands.py
+++ hp_commands.py
@@ -1,6 +1,11 @@
 import pyvisa
 
 class instrument:
+    def assign(self):
+        self.query('IP') # reset to instrument preset
+        self.write('SW1') # swept mode
+        self.write('ME') # display measurement data on CRT
+        self.write('FD0') # output data in ASCII
 
 inst: instrument
 
